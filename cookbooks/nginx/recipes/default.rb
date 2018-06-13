fe5ed183597b3d3ef6006b0d6b498fbdb7eaf2cc
majdarbash <majd.arbash@gmail.com>
2018-06-13 19:09:43 +0400

    *[Chef][NGINX]: latest update

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -73,3 +73,21 @@ bash 'other-configurations' do
     EOH
     creates '/usr/local/scripts/files/other-configurations-done'
 end
+
+bash 'other-configurations' do
+    code <<-EOH
+        echo "Installing node modules ..."
+        ln /usr/bin/nodejs /usr/bin/node -s
+        npm -g install grunt --unsafe-perm
+        npm install -g bower
+        npm install -g phantomjs-prebuilt --unsafe-perm
+
+        echo "Installing Composer ..."
+        cd /tmp
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php ./composer-setup.php --install-dir=/bin --filename=composer
+
+        touch /usr/local/scripts/files/other-configurations-done
+    EOH
+    creates '/usr/local/scripts/files/other-configurations-done'
+end
